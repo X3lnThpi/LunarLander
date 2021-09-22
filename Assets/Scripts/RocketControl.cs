9077797ac7514f85abd55de4db31945161c7719a
Rahul Yadav <devmethyl@outlook.com>
2021-09-22 22:20:15 +0530

    Explored force system for thrusters, very rudimentary

--- Assets/Scripts/RocketControl.cs
+++ Assets/Scripts/RocketControl.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class RocketControl : MonoBehaviour
 {
+    public Rigidbody2D rb2D;
+   // public Rigidbody2D rb2D2;
+    //public GameObject LeftRocket;
+    // public GameObject RightRocket;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,9 @@ public class RocketControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // LeftRocket.GetComponent<Physics2D>();
+        //rb2D = gameObject.AddComponent<Rigidbody2D>();
+        rb2D.AddForce(transform.up * 0.001f);
+       // rb2D2.AddForce(transform.up * 0.001f);
     }
 }
